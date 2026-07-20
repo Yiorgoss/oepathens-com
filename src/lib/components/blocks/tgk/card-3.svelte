06bdf08cb2d64aa3fd8626fc0a8addc4b5d6f685
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2026-07-20 11:49:29 +0300

    another bugfix

--- src/lib/components/blocks/tgk/card-3.svelte
+++ src/lib/components/blocks/tgk/card-3.svelte
@@ -8,7 +8,7 @@
 	const { locale } = $derived(page.params);
 
 	let href = $derived.by(() => {
-		const { urlType, url, reference } = blockData.link || {};
+		const { type: urlType, url, reference } = blockData.link || {};
 		// custom url
 		if (urlType == 'custom' && url) return url;
 		// internal url
