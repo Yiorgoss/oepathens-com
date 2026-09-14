6031ff3e05abc56677d659fcdbcc44748c5289c2
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2026-09-14 07:19:31 +0300

    ticket tailor CSP additions

--- svelte.config.js
+++ svelte.config.js
@@ -27,10 +27,10 @@ const config = {
     }),
     csp: {
       directives: {
-        'script-src': ['self', 'voldemort.calisto.studio', ],
-        'frame-src': ['self', 'https://admin.calisto.studio', ],
-        'img-src': ['self', ],
-        'connect-src': ['self', "api.iconify.design ", 'api.simplesvg.com', 'api.unisvg.com ',],
+        'script-src': ['self', 'voldemort.calisto.studio', 'https://cdn.tickettailor.com'],
+        'frame-src': ['self', 'https://admin.calisto.studio', 'https://*.tickettailor.com',],
+        'img-src': ['self', 'https://app.tickettailor.com', 'https://*.tickettailor.com', 'data:'],
+        'connect-src': ['self', "api.iconify.design ", 'api.simplesvg.com', 'api.unisvg.com ', 'https://*.tickettailor.com'],
         'style-src': ['self', 'unsafe-inline']
       }
     },
